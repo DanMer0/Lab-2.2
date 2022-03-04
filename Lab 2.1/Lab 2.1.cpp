187b7194df04ffe198023076d0718bb70d540fd0
DanMer0 <morkovka.ev@gmail.com>
2022-03-04 17:56:11 +0300

    Добавьте файлы проекта.

---
+++ Lab 2.1/Lab 2.1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+#include <regex>
+#include <set>
+using namespace std::string_literals;
+
+int main()
+{
+    std::string str("apple abby android ananas"); 
+    char c = 'a';// символ 
+    std::regex rc(""s + c);
+    str = std::regex_replace(str, rc, "$& ");
+    std::regex rw("[[:alnum:]]+");
+    std::set<std::string> ss((std::sregex_token_iterator(str.begin(), str.end(), rw)), std::sregex_token_iterator());
+    std::cout << str << '\n' << ss.size() << std::endl;
+}
